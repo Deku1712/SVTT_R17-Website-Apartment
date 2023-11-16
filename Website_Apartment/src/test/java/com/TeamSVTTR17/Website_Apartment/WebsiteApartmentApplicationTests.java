15587d2cffabaa43e5a98e3926031177a04b5b11
Deku1712 <tamtvnde160577@fpt.edu.vn>
2023-11-16 15:24:29 +0700

    inital commit

---
+++ Website_Apartment/src/test/java/com/TeamSVTTR17/Website_Apartment/WebsiteApartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TeamSVTTR17.Website_Apartment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApartmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
